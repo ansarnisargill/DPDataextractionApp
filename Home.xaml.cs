1430a7e092d4d2ae7c1f71f776816482e4428e6e
Ansar <ansarnisargill@gmail.com>
2019-10-05 22:50:50 -0700

    added search

--- Home.xaml.cs
+++ Home.xaml.cs
@@ -12,6 +12,10 @@ using System.Windows.Media;
 using System.Windows.Media.Imaging;
 using System.Windows.Shapes;
 using System.Linq;
+using OfficeOpenXml;
+using Microsoft.Win32;
+using System.IO;
+using OfficeOpenXml.Style;
 
 namespace DPSQLDumpApp
 {
@@ -27,6 +31,7 @@ namespace DPSQLDumpApp
         {
             ConfigHelper.Instance.SetLang("en-us");
             InitializeComponent();
+            window();
         }
         public void window()
         {
@@ -36,27 +41,35 @@ namespace DPSQLDumpApp
         }
         public void RunQuery()
         {
-            var from = CBFrom.SelectedDate.Value;
-            var to = CBTo.SelectedDate.Value;
-            using (var context = new MainContext())
+            try
             {
-                this.ListOfHouses = new List<HouseDTO>();
-                this.ExtractedList = this.ListOfHouses = context.Houses.Where(x => x.PostingDate >= from.Date && x.PostingDate <= to.Date)
-                    .Select(x => new HouseDTO
-                    {
-                        Address = x.Address,
-                        PostingDate = x.PostingDate,
-                        Bedrooms = x.Bedrooms,
-                        City = x.City,
-                        LivingSpaceArea = x.LivingSpaceArea,
-                        LotDimensions = x.LotDimensions,
-                        OriginalURL = x.OriginalURL,
-                        Price = x.Price,
-                        Washrooms = x.Washrooms
-                    }).ToList();
-                SetDGList();
+                var from = CBFrom.SelectedDate.Value;
+                var to = CBTo.SelectedDate.Value;
+                using (var context = new MainContext())
+                {
+                    this.ListOfHouses = new List<HouseDTO>();
+                    this.ExtractedList = this.ListOfHouses = context.Houses.Where(x => x.PostingDate >= from.Date && x.PostingDate <= to.Date)
+                        .Select(x => new HouseDTO
+                        {
+                            Address = x.Address,
+                            PostingDate = x.PostingDate,
+                            Bedrooms = x.Bedrooms,
+                            City = x.City,
+                            LivingSpaceArea = x.LivingSpaceArea,
+                            LotDimensions = x.LotDimensions,
+                            OriginalURL = x.OriginalURL,
+                            Price = x.Price,
+                            Washrooms = x.Washrooms
+                        }).ToList();
+                    SetDGList();
+                }
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show("DB Error");
             }
         }
+
         public void SetDGList()
         {
             dg.ItemsSource = this.ExtractedList;
@@ -79,7 +92,79 @@ namespace DPSQLDumpApp
 
         private void CBFrom_SelectedDateChanged(object sender, SelectionChangedEventArgs e)
         {
+            RunQuery();
+        }
+        public bool ListToExcel<T>(List<T> query)
+        {
+            using (ExcelPackage pck = new ExcelPackage())
+            {
+                //Create the worksheet
+                ExcelWorksheet ws = pck.Workbook.Worksheets.Add("Result");
 
+                //get our column headings
+                var t = typeof(T);
+                var Headings = t.GetProperties();
+                for (int i = 0; i < Headings.Count(); i++)
+                {
+
+                    ws.Cells[1, i + 1].Value = Headings[i].Name;
+                }
+
+                //populate our Data
+                if (query.Count() > 0)
+                {
+                    ws.Cells["A2"].LoadFromCollection(query);
+                }
+
+                //Format the header
+                using (ExcelRange rng = ws.Cells["A1:BZ1"])
+                {
+                    rng.Style.Font.Bold = true;
+                    rng.Style.Fill.PatternType = ExcelFillStyle.Solid;                      //Set Pattern for the background to Solid
+                    rng.Style.Fill.BackgroundColor.SetColor(System.Drawing.Color.FromArgb(79, 129, 189));  //Set color to dark blue
+                    rng.Style.Font.Color.SetColor(System.Drawing.Color.White);
+                }
+
+                //Write it back to the client
+                SaveFileDialog sfd = new SaveFileDialog()
+                {
+                    FileName = "Data", // Default file name
+                    DefaultExt = ".xlsx", // Default file extension
+                    Filter = "Excel Sheet (.xlsx)|*.xlsx" // Filter files by extension
+                };
+                var result = sfd.ShowDialog();
+                if (result == true)
+                {
+                    using (FileStream fs = new FileStream(sfd.FileName, FileMode.Create))
+                    {
+                        pck.SaveAs(fs);
+                    }
+                    return true;
+                }
+                else
+                {
+                    return false;
+                }
+            }
+        }
+        private void TextChanged(object sender, TextChangedEventArgs e)
+        {
+            Search();
+        }
+        public void Search()
+        {
+            var term = TBSearch.Text;
+            this.ExtractedList = new List<HouseDTO>();
+            this.ListOfHouses.ForEach((x) =>
+            {
+                if (x.Address.ToUpper().Contains(term) || x.Bedrooms.ToString().ToUpper().Contains(term) || x.City.ToUpper().Contains(term) || x.LivingSpaceArea.ToUpper().Contains(term) || x.LotDimensions.ToUpper().Contains(term) || x.OriginalURL.ToUpper().Contains(term) || x.PostingDate.ToString().ToUpper().Contains(term) || x.Price.ToUpper().Contains(term) || x.Washrooms.ToString().ToUpper().Contains(term))
+                {
+                    this.ExtractedList.Add(x);
+                }
+            });
+            SetDGList();
         }
     }
+
 }
+
